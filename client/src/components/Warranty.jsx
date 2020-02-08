bb63986a34aecf76f37e0355a4cc07d320236267
TomGower <tom.gower@gmail.com>
2020-02-08 12:40:01 -0600

    Implemented AirBnB style guide on Warranty

--- client/src/components/Warranty.jsx
+++ client/src/components/Warranty.jsx
@@ -1,13 +1,15 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-else-return */
 import React from 'react';
 
-const Warranty = function(props) {
+const Warranty = function WarrantyFunc(props) {
   if (props.id % 19 === 0) {
     return (
-      <img className="tom-warranty-resize" src="../images/WarrantySnip.PNG" alt="Warranty Info"></img>
-    )
+      <img className="tom-warranty-resize" src="../images/WarrantySnip.PNG" alt="Warranty Info" />
+    );
   } else {
     return null;
   }
-}
+};
 
 export default Warranty;
