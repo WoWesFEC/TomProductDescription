9dc672568e90271a1077bec627fd2216170325d7
TomGower <tom.gower@gmail.com>
2020-02-08 12:03:40 -0600

    Implemented AirBnB style guide on SaveOnly

--- client/src/components/SaveOnly.jsx
+++ client/src/components/SaveOnly.jsx
@@ -1,19 +1,28 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/prop-types */
 import React from 'react';
 
-const SaveOnly = function(props) {
-  if (props.saved === false) {
+const SaveOnly = function SaveFunc({ saved, saveFavorite }) {
+  if (saved === false) {
     return (
       <div className="tom-unsaved">
-        <div><img src="../images/unsavedHeart.png" className="tom-heart"></img><a href="#save" className="tom-save-text" onClick={props.saveFavorite}><i aria-hidden="true" className="icon-heart-outline"></i> <span>SAVE</span></a></div>
+        <div>
+          <img src="../images/unsavedHeart.png" className="tom-heart" alt="Clear Unsaved Heart" />
+          <a href="#save" className="tom-save-text" onClick={saveFavorite}>
+            <i aria-hidden="true" className="icon-heart-outline" />
+            {' '}
+            <span>SAVE</span>
+          </a>
+        </div>
       </div>
-    )
-  } else {
-    return (
-      <div className="tom-saved">
-        <img src="../images/savedHeart.png" className="tom-heart"></img><a href="#save" className="tom-save-text"><span>SAVED</span></a>
-      </div>
-    )
+    );
   }
-}
+  return (
+    <div className="tom-saved">
+      <img src="../images/savedHeart.png" className="tom-heart" alt="Red Saved Heart" />
+      <a href="#save" className="tom-save-text"><span>SAVED</span></a>
+    </div>
+  );
+};
 
 export default SaveOnly;
