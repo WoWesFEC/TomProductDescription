14b7434a8bce6c3588811a5d7bddf5c17d410c95
TomGower <tom.gower@gmail.com>
2020-02-08 11:43:33 -0600

    Implemented most of AirBnB style guide on index

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,3 +1,10 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-console */
+/* eslint-disable linebreak-style */
+/* eslint-disable import/extensions */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable linebreak-style */
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
@@ -15,11 +22,10 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      dogs : ['fluffy', 'spot', 'fido'],
-      price : 3.98,
-      name : 'Bob the Dinosaur',
-      productId : 1,
-      descriptions : ['This is really useful', 'You should buy this', 'Buy extra for your friends, if you have some']
+      price: 3.98,
+      name: 'Bob the Dinosaur',
+      productId: 1,
+      descriptions: ['This is really useful', 'You should buy this', 'Buy extra for your friends, if you have some'],
     };
     this.clickSave = this.clickSave.bind(this);
     this.clickShare = this.clickShare.bind(this);
@@ -30,19 +36,19 @@ class App extends React.Component {
 
   componentDidMount() {
     axios.get('http://wowes-env-1.jnkdqwmw8j.us-east-2.elasticbeanstalk.com/items', {
-      params : {
-        ID : this.state.productId
-      }
+      params: {
+        ID: this.state.productId,
+      },
     })
-    .then((result) => {
-      this.setState({
-        price : result.data[0].price.toFixed(2) || 3.98,
-        name : result.data[0].name || 'Bob the Dinosaur'
+      .then((result) => {
+        this.setState({
+          price: result.data[0].price.toFixed(2) || 3.98,
+          name: result.data[0].name || 'Bob the Dinosaur',
+        });
       })
-    })
-    .catch(function(error) {
-      console.log(error)
-    });
+      .catch((error) => {
+        console.log(error);
+      });
     /* WROTE THIS FOR EVENT TESTING PURPOSES
     window.addEventListener('tomCart', (event) => {
       console.log('event heard')
@@ -62,68 +68,92 @@ class App extends React.Component {
 
   getNewProductId(e) {
     console.log('e.dtail', e.detail);
-    this.setState({productId : e.detail});
+    this.setState({ productId: e.detail });
     axios.get('http://wowes-env-1.jnkdqwmw8j.us-east-2.elasticbeanstalk.com/items', {
-      params : {
-        ID : e.detail
-      }
+      params: {
+        ID: e.detail,
+      },
     })
-    .then((result) => {
-      this.setState({
-        price : result.data[0].price.toFixed(2) || 3.98,
-        name : result.data[0].name || 'Bob the Dinosaur'
+      .then((result) => {
+        this.setState({
+          price: result.data[0].price.toFixed(2) || 3.98,
+          name: result.data[0].name || 'Bob the Dinosaur',
+        });
       })
-    })
-    .catch(function(error) {
-      console.log(error)
-    });
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   clickSave() {
+    // eslint-disable-next-line no-console
     console.log(`Saved ${this.state.name}, with id of ${this.state.productId}, or would if we had back-end save functionality.`);
   }
 
   clickShare() {
-    console.log('Tried to share');
     document.getElementById('tom-modal-share').style.display = 'block';
   }
 
   clickShareClose() {
-    console.log('Tried to close share');
     document.getElementById('tom-modal-share').style.display = 'none';
   }
-  
+
   clickStores() {
-    console.log('Tried to display other stores');
     document.getElementById('tom-modal-stores').style.display = 'block';
   }
 
   clickStoresClose() {
-    console.log('Tried to close other stores');
     document.getElementById('tom-modal-stores').style.display = 'none';
   }
 
   render() {
     return (
       <div className="tom-component">
-        <Pricing price={this.state.price} id={this.state.productId} />
-        <LowStock id={this.state.productId} />
-        <Description id={this.state.productId} bullets={this.state.descriptions} />
+        <Pricing
+          price={this.state.price}
+          id={this.state.productId}
+        />
+        <LowStock
+          id={this.state.productId}
+        />
+        <Description
+          id={this.state.productId}
+          bullets={this.state.descriptions}
+        />
         <Warranty id={this.state.productId} />
-        <hr className="tom-ruler"></hr>
+        <hr className="tom-ruler" />
         <div className="tom-grid-100">
-          <ShoppingCart name={this.state.name} price={this.state.price}/>
-          <SaveShare url="http://www.google.com" image0="https://pbs.twimg.com/profile_banners/98042827/1528588486/1080x360" save={this.clickSave} share={this.clickShare} shareClose={this.clickShareClose} id={this.state.productId} name={this.state.name}/>
+          <ShoppingCart name={this.state.name} price={this.state.price} />
+          <SaveShare
+            url="http://www.google.com"
+            image0="https://pbs.twimg.com/profile_banners/98042827/1528588486/1080x360"
+            save={this.clickSave}
+            share={this.clickShare}
+            shareClose={this.clickShareClose}
+            id={this.state.productId}
+            name={this.state.name}
+          />
         </div>
-        <hr className="tom-ruler"></hr>
+        <hr className="tom-ruler" />
         <div className="tom-grid-100">
-          <StoreAvailability id={this.state.productId} stores={this.clickStores} storesClose={this.clickStoresClose} />
-          <ShipAvailability price={this.state.price} id={this.state.productId}/>
+          <StoreAvailability
+            id={this.state.productId}
+            stores={this.clickStores}
+            storesClose={this.clickStoresClose}
+          />
+          <ShipAvailability
+            price={this.state.price}
+            id={this.state.productId}
+          />
         </div>
-        <OtherStoresModal close={this.clickStoresClose} stores={this.clickStores} id={this.state.productId}/>
+        <OtherStoresModal
+          close={this.clickStoresClose}
+          stores={this.clickStores}
+          id={this.state.productId}
+        />
       </div>
-    )
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("tom"));
+ReactDOM.render(<App />, document.getElementById('tom'));
