e043e564196a6edb62b363f54ead6fb8787e99e8
TomGower <tom.gower@gmail.com>
2020-02-08 12:01:34 -0600

    Implemented AirBnB style guide on Pricing

--- client/src/components/Pricing.jsx
+++ client/src/components/Pricing.jsx
@@ -1,20 +1,33 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/prop-types */
 import React from 'react';
 
-const Pricing = function(props) {
-  if (props.id % 15 === 0) {
+const Pricing = function PriceFunc({ id, price }) {
+  if (id % 15 === 0) {
     return (
       <div>
-        <span id="tom-pricing"><span id="tom-dollar-sign">$</span>{(props.price*0.9).toFixed(2)}</span><span id="tom-pricing-secondary">  Was ${props.price}</span><br />
+        <span id="tom-pricing">
+          <span id="tom-dollar-sign">$</span>
+          {(price * 0.9).toFixed(2)}
+        </span>
+        <span id="tom-pricing-secondary">
+          {' '}
+          Was $
+          {price}
+        </span>
+        <br />
         <span id="tom-pricing-discount">SAVE 10% through 02/29/2020</span>
       </div>
-    )
-  } else {
-    return (
-      <div>
-        <span id="tom-pricing"><span id="tom-dollar-sign">$</span>{props.price}</span>
-      </div>
-    )
+    );
   }
+  return (
+    <div>
+      <span id="tom-pricing">
+        <span id="tom-dollar-sign">$</span>
+        {price}
+      </span>
+    </div>
+  );
 };
 
 export default Pricing;
