8669b569a9864be049a4aacbb76617d2d9bfd032
TomGower <tom.gower@gmail.com>
2020-02-08 12:17:02 -0600

    Implemented AirBnB style guide on ShipAvailability

--- client/src/components/ShipAvailability.jsx
+++ client/src/components/ShipAvailability.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-else-return */
 import React from 'react';
 
-const ShipAvailability = function(props) {
+const ShipAvailability = function ShipAvail(props) {
   let deliveryStatus = '';
   if (props.price > 45) {
     deliveryStatus = 'FREE Delivery';
@@ -11,49 +13,49 @@ const ShipAvailability = function(props) {
   if (props.id % 14 === 0) {
     return (
       <div className="tom-grid-ship">
-        <img src="../images/LowesTruck.png" alt="Shipping Truck" className="tom-truck"></img>
+        <img src="../images/LowesTruck.png" alt="Shipping Truck" className="tom-truck" />
         <h4 className="tom-shipping-note">{deliveryStatus}</h4>
         <div>
           <div className="tom-shipping-icon">
-            <img src="../images/NoShipping.png" alt="unavailable"></img>
+            <img src="../images/NoShipping.png" alt="unavailable" />
           </div>
           <div className="tom-shipping-text">
             <span>Currently unavailable</span>
           </div>
         </div>
       </div>
-    )
+    );
   } else if (props.id % 21 === 0) {
     return (
       <div className="tom-grid-ship">
-        <img src="../images/LowesTruck.png" alt="Shipping Truck" className="tom-truck"></img>
+        <img src="../images/LowesTruck.png" alt="Shipping Truck" className="tom-truck" />
         <h4 className="tom-shipping-note">{deliveryStatus}</h4>
         <div>
           <div className="tom-shipping-icon">
-            <img src="../images/greenCheckbox.png" alt="Green Checkbox"></img>
+            <img src="../images/greenCheckbox.png" alt="Green Checkbox" />
           </div>
           <div className="tom-shipping-text">
             <span>Ready for delivery: Estimated on 02/29/2020</span>
           </div>
         </div>
       </div>
-    )
+    );
   } else {
     return (
       <div className="tom-grid-ship">
-        <img src="../images/LowesTruck.png" alt="Shipping Truck" className="tom-truck"></img>
+        <img src="../images/LowesTruck.png" alt="Shipping Truck" className="tom-truck" />
         <h4 className="tom-shipping-note">{deliveryStatus}</h4>
         <div>
           <div className="tom-shipping-icon">
-            <img src="../images/greenCheckbox.png" alt="Green Checkbox"></img>
+            <img src="../images/greenCheckbox.png" alt="Green Checkbox" />
           </div>
           <div className="tom-shipping-text">
-           <span>Available!</span>
+            <span>Available!</span>
           </div>
         </div>
       </div>
-    )
+    );
   }
-}
+};
 
 export default ShipAvailability;
